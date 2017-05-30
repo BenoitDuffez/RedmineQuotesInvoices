bde5725cd6716e0e608a37e981a4f0b4b6839a6e
Benoit Duffez <benoit.duffez@upactivity.com>
2017-05-30 22:34:01 +0200

    added quote entity

---
+++ src/AppBundle/Entity/Quote.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Quote
+ *
+ * @ORM\Table(name="quote")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\QuoteRepository")
+ */
+class Quote
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_creation", type="datetime")
+     */
+    private $dateCreation;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_edition", type="datetime")
+     */
+    private $dateEdition;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="pdf_path", type="string", length=255)
+     */
+    private $pdfPath;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="customer_id", type="integer")
+     */
+    private $customerId;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="project_id", type="integer")
+     */
+    private $projectId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Quote
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set dateCreation
+     *
+     * @param \DateTime $dateCreation
+     *
+     * @return Quote
+     */
+    public function setDateCreation($dateCreation)
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCreation
+     *
+     * @return \DateTime
+     */
+    public function getDateCreation()
+    {
+        return $this->dateCreation;
+    }
+
+    /**
+     * Set dateEdition
+     *
+     * @param \DateTime $dateEdition
+     *
+     * @return Quote
+     */
+    public function setDateEdition($dateEdition)
+    {
+        $this->dateEdition = $dateEdition;
+
+        return $this;
+    }
+
+    /**
+     * Get dateEdition
+     *
+     * @return \DateTime
+     */
+    public function getDateEdition()
+    {
+        return $this->dateEdition;
+    }
+
+    /**
+     * Set pdfPath
+     *
+     * @param string $pdfPath
+     *
+     * @return Quote
+     */
+    public function setPdfPath($pdfPath)
+    {
+        $this->pdfPath = $pdfPath;
+
+        return $this;
+    }
+
+    /**
+     * Get pdfPath
+     *
+     * @return string
+     */
+    public function getPdfPath()
+    {
+        return $this->pdfPath;
+    }
+
+    /**
+     * Set customerId
+     *
+     * @param integer $customerId
+     *
+     * @return Quote
+     */
+    public function setCustomerId($customerId)
+    {
+        $this->customerId = $customerId;
+
+        return $this;
+    }
+
+    /**
+     * Get customerId
+     *
+     * @return int
+     */
+    public function getCustomerId()
+    {
+        return $this->customerId;
+    }
+
+    /**
+     * Set projectId
+     *
+     * @param integer $projectId
+     *
+     * @return Quote
+     */
+    public function setProjectId($projectId)
+    {
+        $this->projectId = $projectId;
+
+        return $this;
+    }
+
+    /**
+     * Get projectId
+     *
+     * @return int
+     */
+    public function getProjectId()
+    {
+        return $this->projectId;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Quote
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
+
