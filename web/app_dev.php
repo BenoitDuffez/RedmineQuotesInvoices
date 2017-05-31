97d0f66d207b1de3cba35fb62d084b41f5c5c076
Benoit Duffez <benoit.duffez@upactivity.com>
2017-05-31 12:39:21 +0200

    allow dev env everywhere

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,7 +8,7 @@ use Symfony\Component\Debug\Debug;
 // for more information
 umask(0000);
 
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
+/*/ This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
@@ -16,7 +16,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-}
+}*/
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../vendor/autoload.php';
