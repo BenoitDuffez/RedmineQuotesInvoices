91bd87616bb9303b7923f65e0ae58a5ca5ff1958
Benoit Duffez <benoit.duffez@upactivity.com>
2017-06-15 08:14:36 +0200

    started to replace invoice code with the sample from the doc

--- src/AppBundle/Form/InvoiceType.php
+++ src/AppBundle/Form/InvoiceType.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\Invoice;
+use AppBundle\Entity\Quote;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -9,6 +11,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use AppBundle\Repository\QuoteRepository;
 
@@ -33,14 +36,41 @@ class InvoiceType extends AbstractType
 					'class' => 'markdown form-control'
 				],
 				'required' => false,
-			])
-			->add('selectedSections', ChoiceType::class, [
-				'mapped' => false,
-				'required' => false,
-				'expanded' => true,
-				'multiple' => true,
-				'choices' => $options['availableSections']
 			]);
+
+		$formModifier = function (FormInterface $form, Quote $quote = null) {
+			$sections = null === $quote ? array() : $quote->getSections();
+
+			$form->add('section', ChoiceType::class, array(
+				'class'       => 'AppBundle:Quote',
+				'placeholder' => '',
+				'multiple'    => true,
+				'expanded' 	  => true,
+				'choices'     => $sections,
+			));
+		};
+
+		$builder->addEventListener(
+			FormEvents::PRE_SET_DATA,
+			function (FormEvent $event) use ($formModifier) {
+				$data = $event->getData();
+
+				$formModifier($event->getForm(), $data->getQuote());
+			}
+		);
+
+		$builder->get('quote')->addEventListener(
+			FormEvents::POST_SUBMIT,
+			function (FormEvent $event) use ($formModifier) {
+				// It's important here to fetch $event->getForm()->getData(), as
+				// $event->getData() will get you the client data (that is, the ID)
+				$quote = $event->getForm()->getData();
+
+				// since we've added the listener to the child, we'll have to pass on
+				// the parent to the callback functions!
+				$formModifier($event->getForm()->getParent(), $quote);
+			}
+		);
     }
     
     /**
